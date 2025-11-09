b1b75e3cf1c1a1490afa97056141cf0bab408373
rubenmz2000 <67756838+rubenmz2000@users.noreply.github.com>
2025-11-09 15:28:00 +0100

    Merge pull request #3
    * feature/addMaterialsCompass (#1)
    
    * upgrade mod_version to 0.0.1

--- src/main/java/com/rmzsoftwares/materialscompass/MaterialsCompass.java
+++ src/main/java/com/rmzsoftwares/materialscompass/MaterialsCompass.java
@@ -1,8 +1,10 @@
 package com.rmzsoftwares.materialscompass;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,9 @@ public class MaterialsCompass {
     public static final String MODID = "materialscompass";
     private static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-    public MaterialsCompass() {
+    public MaterialsCompass(FMLJavaModLoadingContext modLoadingContext) {
+        IEventBus bus = modLoadingContext.getModEventBus();
+        ModItems.ITEMS.register(bus);
         LOGGER.info("Materials Compass mod initialized!");
     }
 
