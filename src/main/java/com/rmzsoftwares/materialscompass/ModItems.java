f9d64fb7ea56a9bb29c81c6062dc0fe74591db5c
ruben <rubenmz2000@gmail.com>
2025-11-09 11:36:38 +0100

    Add Mineral Compass item and localization support

---
+++ src/main/java/com/rmzsoftwares/materialscompass/ModItems.java
@@ -0,0 +1,14 @@
+package com.rmzsoftwares.materialscompass;
+
+import com.rmzsoftwares.materialscompass.items.MineralCompassItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MaterialsCompass.MODID);
+
+    public static final RegistryObject<Item> MINERAL_COMPASS = ITEMS.register("mineral_compass", () -> new MineralCompassItem(new Item.Properties().stacksTo(1)));
+}
